d8a8da33b3f8d80ce69b93fc0cafde77b91b8bea
Boadu Aaron Jeffrey Bosomepem <jeffreyaaron84@gmail.com>
2025-01-17 10:31:32 +0000

    Refactor product controller to improve error handling and streamline product addition; update search results display logic for better user experience

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,31 +1,33 @@
 const { v4: uuidv4 } = require("uuid");
-const {products} = require("../models/products.models");
-
-
+const { products } = require("../models/products.models");
 
 function postProduct(req, res) {
-  const { name, product_name, product_price, product_description } =
-    req.body;
-    console.log(req.file, req.body);
+  try {
+    const { name, product_name, product_price, product_description } = req.body;
+    // console.log(req.file, req.body);
+
+    if (!name || !req.file || !product_name || !product_price) {
+      return res.status(400).json({
+        error: "Data invalid",
+      });
+    }
+
     const imagePath = `/images/products/${req.file.filename}`;
-
-  if (!name || !req.file || !product_name || !product_price) {
-    return res.status(400).json({
-      error: "Data invalid",
-    });
+    const newProduct = {
+      id: uuidv4(),
+      name,
+      image: imagePath,
+      product_name,
+      product_price,
+      product_description,
+    };
+    products.push(newProduct);
+    console.log(products)
+    
+    res.redirect("/");
+  } catch (err) {
+    res.status(400).send({ error: err.message });
   }
-
-  const newProduct = {
-    id: uuidv4(),
-    name,
-    image: imagePath,
-    product_name,
-    product_price,
-    product_description,
-  };
-  products.push(newProduct);
-
-  res.redirect('/');
 }
 
 module.exports = {
